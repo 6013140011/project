dfd3282cdf94893dd77c9ff4c5e0a833e0157413
achitphon <60141110010@mutacth.com>
2020-02-17 22:28:01 +0700

    1st

---
+++ resources/views/menu.blade.php
@@ -0,0 +1,32 @@
+<center> 
+<div class="row text-center">
+<body background="test2.jpg">
+<br>
+<br>
+
+    <img  src="logo.jpg" width="750" height="400" >
+    <br>
+    <table width="500" border="15">
+
+<tr>
+<td colspan="2" style="background-color:black">
+<center> 
+<h1><font color="white"> Menu </font> </h1>
+<center> 
+</td>
+</tr>
+
+<tr>
+<td style="background-color:white;width:100px;text-align:top;">
+
+Test1<br>
+Test2<br>
+Test3<br>
+Test4
+
+</td>
+
+      
+   
+
+<center>    
